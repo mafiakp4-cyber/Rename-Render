cf17af2519398b016cc0e2437f6e51fcdf351665
mafiakp4-cyber <mafiakp4@gmail.com>
2025-09-23 23:04:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,21 +7,21 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "")
+API_ID = os.environ.get("API_ID", "21302239")
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "1560930c983fbca6a1fcc8eab760d40d")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "VJ_Botz") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "https://t.me/+Tbkw7GQzcB05M2U9") 
 
              # Don't Remove Credit @VJ_Botz
              # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
              # Ask Doubt on telegram @KingVJ01
 
-DB_NAME = os.environ.get("DB_NAME", "renamevjbot")     
+DB_NAME = os.environ.get("DB_NAME", "kanha")     
 
-DB_URL = os.environ.get("DB_URL", "")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://Kanha:kanha54321@kanha.hnytqah.mongodb.net/Kanha?retryWrites=true&w=majority&appName=Kanha"")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
